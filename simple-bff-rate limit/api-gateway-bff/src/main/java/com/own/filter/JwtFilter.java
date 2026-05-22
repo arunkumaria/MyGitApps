421074e743ce932b5f23b3fdabe83529df468ce4
arunkumaria <92155246+arunkumaria@users.noreply.github.com>
2026-05-22 13:31:16 +0530

    bff with rate limiting
    api, auth, rate included

--- simple-bff-rate limit/api-gateway-bff/src/main/java/com/own/filter/JwtFilter.java
+++ simple-bff-rate limit/api-gateway-bff/src/main/java/com/own/filter/JwtFilter.java
@@ -1,19 +1,25 @@
 package com.own.filter;
 
-import java.nio.charset.StandardCharsets;
-
-import javax.crypto.SecretKey;
-
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+
+import org.springframework.core.Ordered;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtFilter implements GlobalFilter, Ordered {
@@ -31,7 +37,8 @@ public class JwtFilter implements GlobalFilter, Ordered {
             ServerWebExchange exchange,
             GatewayFilterChain chain
     ) {
-    	String path =
+
+        String path =
                 exchange.getRequest()
                         .getURI()
                         .getPath();
@@ -54,7 +61,8 @@ public class JwtFilter implements GlobalFilter, Ordered {
             return exchange.getResponse().setComplete();
         }
 
-        String token = authHeader.substring(7);
+        String token =
+                authHeader.substring(7);
 
         try {
 
