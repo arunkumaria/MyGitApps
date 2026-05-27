7acb0c201c0c48e1de92953a3f3a1aec4b88a507
arunkumaria <92155246+arunkumaria@users.noreply.github.com>
2026-05-27 17:25:43 +0530

    commit on zipkin for bff arc app
    zipkin in simple bff

---
+++ simple-bff-zipkin/food-app-microservices/restaurant-service-bff/src/main/java/com/own/controller/RestaurantController.java
@@ -0,0 +1,29 @@
+package com.own.controller;
+
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Map<String, Object>> restaurants() {
+
+        return List.of(
+
+                Map.of(
+                        "id", 1,
+                        "name", "Dominos"
+                ),
+
+                Map.of(
+                        "id", 2,
+                        "name", "KFC"
+                )
+        );
+    }
+}
